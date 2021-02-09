3a6bf0261f97f2852fcffea62d80dfbbd9df9e47
Ckyiu <UnsignedArduino@outlook.com>
2021-02-09 13:18:09 -0500

    Allow expandable arguments

--- achievements.ts
+++ achievements.ts
@@ -13,15 +13,19 @@ namespace Achievements {
      * @param icon: The icon of the achievement when shown to the player, must be 8x8 otherwise ignored.
      * @return: Returns a boolean depending on whether this is the first time the condition is true.
      */
-    //% block="check for achievement named $achievementName in condition $condition and display title as $displayName and description as $displayDescription and icon as $icon"
+    //% block="check for achievement named $achievementName in condition $condition and display title as $displayName||and description as $displayDescription|and icon as $icon"
     //% icon.shadow=screen_image_picker
+    //% expandableArgumentMode="enabled"
     //% weight=90
     export function checkForAchievement(achievementName: string, 
                                         condition: boolean = false, 
                                         displayName: string,
-                                        displayDescription: string = "",
+                                        displayDescription?: string,
                                         icon?: Image){
         achievementName = "achievement_" + achievementName
+        if (!(displayDescription)) {
+            displayDescription = ""
+        }
         let value = 1
         if (!(condition)) {
             value = 0
@@ -49,13 +53,14 @@ namespace Achievements {
      * @param displayDescription: The discription of the achievement that is shown to the player - optional.
      * @param icon: The icon of the achievement when shown to the player, must be 8x8 otherwise ignored.
      */
-    //% block="check for achievement named $achievementName in condition $condition and display title as $displayName and description as $displayDescription and icon as $icon"
+    //% block="check for achievement named $achievementName in condition $condition and display title as $displayName||and description as $displayDescription|and icon as $icon"
     //% icon.shadow=screen_image_picker
+    //% expandableArgumentMode="enabled"
     //% weight=100
     export function checkForAchievementNoReturn(achievementName: string, 
                                                 condition: boolean = false, 
                                                 displayName: string,
-                                                displayDescription: string = "",
+                                                displayDescription?: string,
                                                 icon?: Image) {
         checkForAchievement(achievementName, condition, displayName, displayDescription, icon)
     }
